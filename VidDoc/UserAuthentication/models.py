b560b420ed441d5acf6f24f7e2adbf12a0762171
Varun487 <seshuvarun@gmail.com>
2021-03-30 11:46:44 +0530

    Completed user models

--- VidDoc/UserAuthentication/models.py
+++ VidDoc/UserAuthentication/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 from datetime import datetime
 
+
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=200)
-    email = models.TextField()
+    email = models.CharField(max_length=500)
     password = models.CharField(max_length=64)
     address = models.TextField()
-    age = models.TextField()
-    city = models.TextField()
-    phnumber = models.TextField()
+    age = models.IntegerField()
+    city = models.CharField(max_length=500)
+    phone_number = models.CharField(max_length=500)
     created_at = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.name
+
+
+class Doctor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    speciality = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.user.name
